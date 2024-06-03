21db4a3ed90d5a075cff0da7d66464fec6facaef
fess986u <fess986@mail.ru>
2024-06-03 09:14:37 +0300

    Установка в проект зависимости config  #2

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import config from 'config';
 
 const app = express();
-const port = 5000;
+console.log(config.get('ENV'));
+
+const port = config.get('PORT') || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
