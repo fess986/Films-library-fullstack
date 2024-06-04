66ca21623c8f7a8ca9187dd1f61fd464156345cf
fess986u <fess986@mail.ru>
2024-06-04 20:21:21 +0300

    Редирект на главную страницу  #10

--- frontend/src/hooks/useRoutes.tsx
+++ frontend/src/hooks/useRoutes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import { AppRoutes } from '../const/const'
 import { FilmProps } from '../mock/films'
@@ -24,93 +24,93 @@ import HeaderMain from '../Components/Layout/Header/HeaderMain/HeaderMain'
 import FooterMain from '../Components/Layout/Footer/FooterMain/FooterMain'
 import BodyMain from '../Components/Layout/Body/BodyMain/BodyMain'
 
-export default function useRoutes (isAuth : boolean, films : FilmProps[])  {
+export default function useRoutes(isAuth: boolean, films: FilmProps[]) {
   console.log(isAuth);
 
   return (
     <Routes>
-    <Route path={AppRoutes.ROOT} element={<Layout
-      HeaderComponent={HeaderMain} 
-      FooterComponent={FooterMain}
-      BodyComponent={BodyMain}
-      headerProps={{}}
-      footerProps={{}}
-      bodyProps={{}} />}>
+      <Route path={AppRoutes.ROOT} element={<Layout
+        HeaderComponent={HeaderMain}
+        FooterComponent={FooterMain}
+        BodyComponent={BodyMain}
+        headerProps={{}}
+        footerProps={{}}
+        bodyProps={{}} />}>
 
-      <Route
-        index
-        element={<Main films={films}/>}
-      />
+        <Route
+          index
+          element={<Main films={films} />}
+        />
 
-      <Route
-        path={AppRoutes.MAIN}
-        element={<Main films={films}/>}
-      />
+        <Route
+          path={AppRoutes.MAIN}
+          element={<Main films={films} />}
+        />
 
-      <Route
-        path={AppRoutes.FILM_CARD}
-        element={<FilmCard />}
-      />
-    </Route>
+        <Route
+          path={AppRoutes.FILM_CARD}
+          element={<FilmCard />}
+        />
+      </Route>
 
-    <Route path={AppRoutes.ROOT} element={<Layout
-      HeaderComponent={HeaderReview} 
-      FooterComponent={FooterReview}
-      BodyComponent={BodyReview}
-      headerProps={{}}
-      footerProps={{}}
-      bodyProps={{}}  
-    />}>
-      <Route
-        path={AppRoutes.ADD_REVIEW}
-        element={<AddReview />
-        }
-      />
-    </Route>
+      <Route path={AppRoutes.ROOT} element={<Layout
+        HeaderComponent={HeaderReview}
+        FooterComponent={FooterReview}
+        BodyComponent={BodyReview}
+        headerProps={{}}
+        footerProps={{}}
+        bodyProps={{}}
+      />}>
+        <Route
+          path={AppRoutes.ADD_REVIEW}
+          element={<AddReview />
+          }
+        />
+      </Route>
 
-    <Route path={AppRoutes.ROOT} element={<Layout
-      HeaderComponent={HeaderSignIn} 
-      FooterComponent={FooterSignIn}
-      BodyComponent={BodySignIn}
-      headerProps={{}}
-      footerProps={{}}
-      bodyProps={{}}  
-    />}>
+      <Route path={AppRoutes.ROOT} element={<Layout
+        HeaderComponent={HeaderSignIn}
+        FooterComponent={FooterSignIn}
+        BodyComponent={BodySignIn}
+        headerProps={{}}
+        footerProps={{}}
+        bodyProps={{}}
+      />}>
 
-      <Route
-        path={AppRoutes.SIGN_IN}
-        element={<SignIn />}
-      />
+        <Route
+          path={AppRoutes.SIGN_IN}
+          element={<SignIn />}
+        />
 
-      <Route
-        path={AppRoutes.MY_LIST}
-        element={<MyList />
-        }
-      />
+        <Route
+          path={AppRoutes.MY_LIST}
+          element={<MyList />
+          }
+        />
+      </Route>
 
-    </Route>
+      <Route path='/' element={<Layout
+        HeaderComponent={HeaderPlayer}
+        FooterComponent={FooterPlayer}
+        BodyComponent={BodyPlayer}
+        headerProps={{}}
+        footerProps={{}}
+        bodyProps={{}}
+      />}>
 
-    <Route path='/' element={<Layout
-      HeaderComponent={HeaderPlayer} 
-      FooterComponent={FooterPlayer}
-      BodyComponent={BodyPlayer}
-      headerProps={{}}
-      footerProps={{}}
-      bodyProps={{}}  
-    />}>
+        <Route
+          path={AppRoutes.PLAYER}
+          element={<Player />}
+        />
 
-      <Route
-        path={AppRoutes.PLAYER}
-        element={<Player />}
-      />
-
-    </Route>
-
-    <Route
+      </Route>
+      {/* <Route
       path='*'
       element={<Page404 />}
-    />
+    /> */}
 
-  </Routes>
+      <Route path="*" element={<Navigate to={AppRoutes.MAIN} replace />} />
+
+    </Routes>
   )
 }
