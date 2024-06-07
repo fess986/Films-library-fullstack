187333438028fff99b236c9c49370666d7a3ba12
fess986u <fess986@mail.ru>
2024-06-07 12:40:31 +0300

    Рефакторинг Layout - footer #10
    - так как footer в приложении будет везде
    одиноковый, убраны лишние реализации
    - на странице Player - футера вообще нет, учитывая
    это - исправлен Layout
    - изменён главный блок для рендеринга на div
    (раньше был body) - из-за возможных сайд-
    эффектов сторонних приложений по совету
    самого реакта

--- frontend/src/Components/Layout/Footer/Footer.tsx
+++ frontend/src/Components/Layout/Footer/Footer.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
-export interface FooterProps {
-  text?: string;
-}
-
-const Footer: React.FC<FooterProps> = ({ text }) => {
-  return <footer>{text}</footer>;
+const Footer: React.FC = () => {
+  return (
+    <footer className="main-page__footer footer">
+      <a className="footer__logo logo">
+        <img className="logo__image" src="/images/logo.jpeg" alt="Films Library" />
+      </a>
+      <div className="footer__copyright copypright">
+        <p>Fess inc@</p>
+      </div>
+    </footer>
+  )
 };
 
 export default Footer;
