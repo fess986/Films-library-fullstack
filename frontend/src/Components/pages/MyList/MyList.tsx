3da5ade5645579d8489abcbb4de1b93ab7ba3868
fess986u <fess986@mail.ru>
2024-06-07 15:18:46 +0300

    Вёрстка компонента my-list #18

--- frontend/src/Components/pages/MyList/MyList.tsx
+++ frontend/src/Components/pages/MyList/MyList.tsx
@@ -1,5 +1,82 @@
-const MyList: React.FC = ( ) => {
-  return <h1>MyList</h1>;
+const MyList: React.FC = () => {
+  return (
+    <div className="main-page__content main-page-content">
+      <section className="main-page-content__catalog catalog">
+        <h2 className="catalog__title visually-hidden">My list</h2>
+        <h2 className="catalog__title">My list</h2>
+        <div className="catalog__films-list films-list">
+          <article className="films-list__films-card small-film-card">
+            <div className="small-film-card__image">
+              <img className="small-film-card__img" src="/images/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt="Fantastic Beasts: The Crimes of Grindelwald" />
+            </div>
+            <h3 className="small-film-card__title">
+              <a className="small-film-card__link" href="film-page.html">Fantastic Beasts: The Crimes of Grindelwald</a>
+            </h3>
+          </article>
+          <article className="films-list__films-card small-film-card">
+            <div className="small-film-card__image">
+              <img className="small-film-card__img" src="/images/bohemian-rhapsody.jpg" alt="Bohemian Rhapsody" />
+            </div>
+            <h3 className="small-film-card__title">
+              <a className="small-film-card__link" href="film-page.html">Bohemian Rhapsody</a>
+            </h3>
+          </article>
+          <article className="films-list__films-card small-film-card">
+            <div className="small-film-card__image">
+              <img className="small-film-card__img" src="/images/macbeth.jpg" alt="Macbeth" />
+            </div>
+            <h3 className="small-film-card__title">
+              <a className="small-film-card__link" href="film-page.html">Macbeth</a>
+            </h3>
+          </article>
+          <article className="films-list__films-card small-film-card">
+            <div className="small-film-card__image">
+              <img className="small-film-card__img" src="/images/aviator.jpg" alt="Aviator" />
+            </div>
+            <h3 className="small-film-card__title">
+              <a className="small-film-card__link" href="film-page.html">Aviator</a>
+            </h3>
+          </article>
+          <article className="films-list__films-card small-film-card">
+            <div className="small-film-card__image">
+              <img className="small-film-card__img" src="/images/we-need-to-talk-about-kevin.jpg" alt="We need to talk about Kevin" />
+            </div>
+            <h3 className="small-film-card__title">
+              <a className="small-film-card__link" href="film-page.html">We need to talk about Kevin</a>
+            </h3>
+          </article>
+          <article className="films-list__films-card small-film-card">
+            <div className="small-film-card__image">
+              <img className="small-film-card__img" src="/images/what-we-do-in-the-shadows.jpg" alt="What We Do in the Shadows" />
+            </div>
+            <h3 className="small-film-card__title">
+              <a className="small-film-card__link" href="film-page.html">What We Do in the Shadows</a>
+            </h3>
+          </article>
+          <article className="films-list__films-card small-film-card">
+            <div className="small-film-card__image">
+              <img className="small-film-card__img" src="/images/revenant.jpg" alt="Revenant" />
+            </div>
+            <h3 className="small-film-card__title">
+              <a className="small-film-card__link" href="film-page.html">Revenant</a>
+            </h3>
+          </article>
+          <article className="films-list__films-card small-film-card">
+            <div className="small-film-card__image">
+              <img className="small-film-card__img" src="/images/johnny-english.jpg" alt="Johnny English" />
+            </div>
+            <h3 className="small-film-card__title">
+              <a className="small-film-card__link" href="film-page.html">Johnny English</a>
+            </h3>
+          </article>
+        </div>
+        <div className="catalog__more-films-button more-films-button">
+          <button className="more-films__button" type="button">Show more</button>
+        </div>
+      </section>
+    </div>
+
+  )
 };
 
 export default MyList;
--- frontend/src/Components/pages/SignIn/SignIn.tsx
+++ frontend/src/Components/pages/SignIn/SignIn.tsx
@@ -1,31 +1,35 @@
 const SignIn: React.FC = () => {
   return (
-    <section className="sign-in__form-container form-container">
-      <div className="form-container--bottom">
-        <div className="form-container--top">
-          <form action="#" className="sign-in__form sign-in">
-            <p className="sign-in__message-text message-text message-text--normal">
-              Please sign in if you have an account or sign up if you don’t
-            </p>
-            {/* <p class="sign-in__message-text message-text message-text--error">
+    <>
+      <h2 className="sign-in__title title">Sign in</h2>
+
+      <section className="sign-in__form-container form-container">
+        <div className="form-container--bottom">
+          <div className="form-container--top">
+            <form action="#" className="sign-in__form sign-in">
+              <p className="sign-in__message-text message-text message-text--normal">
+                Please sign in if you have an account or sign up if you don’t
+              </p>
+              {/* <p class="sign-in__message-text message-text message-text--error">
 								We can’t recognize this email and password combination. Please
 								try again.
 							</p> */}
-            <div className="sign-in__fields">
-              <div className="sign-in__field">
-                <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" />
-                <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
+              <div className="sign-in__fields">
+                <div className="sign-in__field">
+                  <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" />
+                  <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
+                </div>
+                <div className="sign-in__field">
+                  <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" />
+                  <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
+                </div>
               </div>
-              <div className="sign-in__field">
-                <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" />
-                <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
-              </div>
-            </div>
-            <button className="more-films__button sign-in__button" type="submit">Sign in</button>
-          </form>
+              <button className="more-films__button sign-in__button" type="submit">Sign in</button>
+            </form>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   )
 };
 
