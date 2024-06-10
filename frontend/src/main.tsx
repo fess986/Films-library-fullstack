3ea23d6f444778d394e6a6bf0562417cde373079
fess986u <fess986@mail.ru>
2024-06-10 19:14:42 +0300

    Подключены глобальные стили к проекту #44

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,14 @@ import App from './App.tsx'
 import './styles/styles.css'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './Components/theme/default.ts'
+import GlobalStyles from './globalStyles.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
       <App />
+      <GlobalStyles />
     </ThemeProvider >
   </React.StrictMode>,
 )
