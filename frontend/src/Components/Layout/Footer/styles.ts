086f764e5ba1ecbacafc4cab247514881f54827d
fess986u <fess986@mail.ru>
2024-06-12 12:47:56 +0300

    Footer Layout - Styled #48

---
+++ frontend/src/Components/Layout/Footer/styles.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import { Div } from "../../styled/Components/Div/Div";
+import { Footer } from "../../styled/Components/Footer/Footer";
+import { fontMedium, textColorAdditional } from "../../styled/Mixins/mixins";
+
+export const DivCopyright = styled(Div)`
+	${fontMedium}
+	${textColorAdditional}
+`;
+
+export const FooterPage = styled(Footer)`
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+
+	max-width: 1400px;
+`;
+
+export const FooterMain = styled(FooterPage)`
+	margin-top: 100px;
+	margin-bottom: 20px;
+`;
+
+export const FooterSignIn = styled(FooterPage)`
+	margin-bottom: 20px;
+`;
---
+++ frontend/src/Components/styled/Components/Footer/Footer.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Footer = styled.footer`
+  display: block;
+
+  margin: 0;
+  padding: 0;
+  border: 0;
+
+  font-size: 100%;
+  font: inherit;
+`
