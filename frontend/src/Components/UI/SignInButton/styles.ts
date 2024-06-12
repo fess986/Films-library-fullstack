e6afb39a573bbd6ee29d85ce8f058b3b78aaa17f
fess986u <fess986@mail.ru>
2024-06-12 13:17:23 +0300

    SignInButton Component - styled #48

---
+++ frontend/src/Components/UI/SignInButton/styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import { StyledLink } from "../../styled/Components/Link/Link";
+
+export const LinkSignIn = styled(StyledLink)`
+color: ${(props) => props.theme.textColorMainDark};
+`
