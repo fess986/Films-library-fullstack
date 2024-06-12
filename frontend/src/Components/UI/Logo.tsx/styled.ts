40b24a52c1f3f564976abbd62fb0ed5ccb073926
fess986u <fess986@mail.ru>
2024-06-12 11:06:00 +0300

    Logo - трансформирован в styled-components #48

---
+++ frontend/src/Components/UI/Logo.tsx/styled.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { StyledLink } from "../../styled/Components/Link/Link";
+import { Img } from "../../styled/Components/Img/Img";
+import { zIndexTop } from "../../styled/Mixins/mixins";
+
+type LogoProps = {
+	footer?: boolean;
+};
+
+export const LinkLogo = styled(StyledLink)<LogoProps>`
+display: block;
+width: 63px;
+height: 63px;
+border-radius: 50%;
+overflow: hidden;
+cursor: pointer;
+
+margin-right: ${(props) => (props.footer ? "20px" : "0")};
+
+will-change: transform; // говорим браузеру что будет изменяться размер
+transition: 0.5s transform, 0.5s -webkit-transform; // задаём время трансформации
+
+&:hover {
+${zIndexTop}// для того чтобы изображение перекрывало другие смежные элементы
+  transform: scale(1.2); // задаём масштаб трансформирования
+`;
+export const ImgLogo = styled(Img)`
+	width: 100%;
+	height: 100%;
+	object-fit: contain;
+`;
