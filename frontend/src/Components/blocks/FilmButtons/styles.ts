0071cfad06ada1e43f7fee81cc2a2e3addd08e38
fess986u <fess986@mail.ru>
2024-06-13 16:50:02 +0300

    FilmButtons block styled  #48

---
+++ frontend/src/Components/blocks/FilmButtons/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { Div } from "../../styled/Components";
+
+
+export const DivFilmButtons = styled(Div)`
+	display: flex;
+	max-width: 700px;
+	max-height: 300px;
+	overflow: hidden;
+`
+
