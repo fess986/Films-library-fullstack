ae701dd30c9c35424e5e073edc0e1f541ebe7443
fess986u <fess986@mail.ru>
2024-06-20 20:00:32 +0300

    FilmDetails block styled #73

--- frontend/src/Components/blocks/FilmCard/FilmDetails/FilmDetails.tsx
+++ frontend/src/Components/blocks/FilmCard/FilmDetails/FilmDetails.tsx
@@ -1,14 +1,38 @@
 import { FilmProps } from "../../../../types/types";
+import { DivDetailsContainer, DivItem, SpanDetailsItemTitle, SpanDetailsItemValue } from "./styles";
 
 type FilmDetailsProps = {
   film: FilmProps
 }
 
-const FilmDetails: React.FC<FilmDetailsProps> = ({film}) => {
+const FilmDetails: React.FC<FilmDetailsProps> = ({ film }) => {
   return (
-    <>
-      film details
-    </>
+    <DivDetailsContainer>
+      <DivItem>
+        <SpanDetailsItemTitle>Director:</SpanDetailsItemTitle>
+        <SpanDetailsItemValue>{film.director}</SpanDetailsItemValue>
+      </DivItem>
+
+      <DivItem>
+        <SpanDetailsItemTitle>Starring:</SpanDetailsItemTitle>
+        <SpanDetailsItemValue>{film.starring.join(", ")}</SpanDetailsItemValue>
+      </DivItem>
+
+      <DivItem>
+        <SpanDetailsItemTitle>Run Time:</SpanDetailsItemTitle>
+        <SpanDetailsItemValue>1h 34m</SpanDetailsItemValue>
+      </DivItem>
+
+      <DivItem>
+        <SpanDetailsItemTitle>Genre:</SpanDetailsItemTitle>
+        <SpanDetailsItemValue>{film.genre.join(", ")}</SpanDetailsItemValue>
+      </DivItem>
+
+      <DivItem>
+        <SpanDetailsItemTitle>Released:</SpanDetailsItemTitle>
+        <SpanDetailsItemValue>{film.released}</SpanDetailsItemValue>
+      </DivItem>
+    </DivDetailsContainer>
   )
 }
 
---
+++ frontend/src/Components/blocks/FilmCard/FilmDetails/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import { Div, Span } from "../../../styled/Components";
+import { textColorAdditional, fontMedium } from "../../../styled/Mixins/mixins";
+
+export const DivDetailsContainer = styled(Div)`
+	max-width: 792px;
+`;
+
+export const DivItem = styled(Div)`
+  margin-bottom: 30px;
+`
+export const SpanDetailsItemTitle = styled(Span)`
+${fontMedium}
+${textColorAdditional}
+font-weight: 700;
+
+margin-right: 10px;
+`
+export const SpanDetailsItemValue = styled(Span)`
+${fontMedium}
+${textColorAdditional}
+`
