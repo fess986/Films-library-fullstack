3d9bdddc4cfc4107b99a5759238809a14f67a691
fess986u <fess986@mail.ru>
2024-07-04 18:39:03 +0300

    Breadcrumbs блок добавлен в styled-components  #95

--- frontend/src/Components/Layout/Header/HeaderReview/HeaderReview.tsx
+++ frontend/src/Components/Layout/Header/HeaderReview/HeaderReview.tsx
@@ -18,37 +18,7 @@ const HeaderReview: React.FC<HeaderMainProps> = ({ isAuth, currentFilm }) => {
       <HeroPicture $page={PageList.ADD_REVIEW}/>
 
       <H1Hidden>Add review</H1Hidden>
-
       <Header isAuth={isAuth} breadcrumbs $page={PageList.ADD_REVIEW}/>
-
-      {/* <header className="add-review-page-header hero__header">
-        <div className="main-page-header__logo">
-          <a className="logo">
-            <img className="logo__image" src="/images/logo.jpeg" alt="Films Library" />
-          </a>
-          <nav className="main-page-header__breadcrumbs breadcrumbs">
-            <ul className="breadcrumbs__list">
-              <li className="breadcrumbs__item">
-                <a href="/" className="breadcrumbs__link">The Grand Budapest Hotel</a>
-              </li>
-              <li className="breadcrumbs__item">
-                <a href="/" className="breadcrumbs__link">Add review</a>
-              </li>
-            </ul>
-          </nav>
-
-        </div>
-        <ul className="main-page-header__user-block user-block">
-          <li className="user-block__item">
-            <div className="user-block__avatar user-avatar">
-              <img className="user-avatar__image" src="/images/avatar.png" alt="User avatar" />
-            </div>
-          </li>
-          <li className="user-block__item user-block-text">
-            <a className="user-block__link">Sign out</a>
-          </li>
-        </ul>
-      </header> */}
 
       <div className="add-review__film-card film-card">
         <div className="movie-page__film-card-info film-card__info">
---
+++ frontend/src/Components/blocks/Breadcrumbs/styles.ts
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+import { StyledLink, Li, Nav, Ul } from "../../styled/Components";
+import { fontMedium, textColorMain } from "../../styled/Mixins/mixins";
+
+export const BreadcrumbLink = styled(StyledLink)<{ $active?: boolean }>`
+	display: inline-block;
+	height: ${(props) => props.theme.textSizeMedium};
+	max-width: 450px;
+	overflow-x: clip;
+	white-space: nowrap;
+	text-overflow: ellipsis;
+
+	${fontMedium}
+	${textColorMain}
+
+  transition: transform .5s linear;
+
+  ${(props) => props.$active && `color: ${props.theme.textColorMainDark}`};
+
+	&:hover {
+		transform: scale(1.1);
+	}
+`;
+
+export const BreadcrumbLi = styled(Li)`
+	position: relative;
+	display: inline-block;
+	max-width: 500px;  
+  ${textColorMain}
+
+	&:not(:last-child)::after {
+		content: "/";
+		margin-left: 5px;
+		margin-right: 7px;
+		@include font-medium;
+	}
+`;
+
+// .breadcrumbs__item {
+// 		&--my-list {
+// 			color: $text-color-additional;
+// 		}
+
+export const BreadcrumbsList = styled(Ul)`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	flex-wrap: wrap;
+`
+
+export const NavBreadcrumbs = styled(Nav)`
+margin-left: 20px;
+max-width: 700px;
+`
