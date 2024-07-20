bf3659806295b06d4d7d45b617b1d9352025ea65
fess986u <fess986@mail.ru>
2024-07-20 10:52:00 +0300

    Улучшена логика работы TS в Layout #14
    - добавлена типизация компонентов и их пропсов
    через дженерики, для их совпадения, для того
    чтобы TS мог правильно сопостовлять их и
    давать корректные подсказки

--- frontend/src/Components/Layout/Layout.tsx
+++ frontend/src/Components/Layout/Layout.tsx
@@ -1,32 +1,5 @@
 import React, { ReactNode } from 'react';
-// import { HeaderProps } from './Header/Header';
-// import { BodyProps } from './Body/Body';
 import { Outlet } from 'react-router-dom';
-
-// interface LayoutProps<HeaderProps, BodyProps> {
-//   HeaderComponent?: React.FC<HeaderProps>;
-//   FooterComponent?: React.FC;
-//   BodyComponent: React.FC<BodyProps>;
-//   headerProps?: HeaderProps;
-//   bodyProps?: BodyProps;
-//   children?: ReactNode;
-// }
-
-// const Layout  = <HeaderProps, BodyProps> ({
-//   HeaderComponent, 
-//   FooterComponent,
-//   BodyComponent,
-//   headerProps = {} as HeaderProps, 
-//   bodyProps = {} as BodyProps, 
-// }) : LayoutProps<HeaderProps, BodyProps> => {
-//   return (
-//     <BodyComponent {...bodyProps}>
-//       {HeaderComponent &&  <HeaderComponent {...headerProps}/>}
-//       <Outlet />
-//       {FooterComponent &&  <FooterComponent />}
-//     </BodyComponent>
-//   );
-// };
 
 interface LayoutProps<HP, BP> {
   HeaderComponent?: React.FC<HP>;
@@ -46,8 +19,8 @@ const Layout = <HP, BP>({
 }: LayoutProps<HP, BP>) => {
   return (
     <BodyComponent {...bodyProps as BP}>
-      // @ts-expect-error
-      {HeaderComponent &&
+      { 
+      HeaderComponent &&
         // @ts-expect-error HeaderComponent - ругается на то, что возможные передаваемые параметры не поддерживаются, если пробовать её исправить, потом при использовании компонента Layout все компоненты нужно будет приводить к этому типу, что очень сильно тормозит разработку, и в итоге TS уже не помогает, а мешает разработке
         <HeaderComponent
           {...headerProps} />}
