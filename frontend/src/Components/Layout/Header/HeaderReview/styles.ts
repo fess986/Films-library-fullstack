7df6247fc8f49147fac7b0db44045cc81ac4ebcd
fess986u <fess986@mail.ru>
2024-07-21 09:57:44 +0300

    Стилизация компонента HeaderReview #95

--- frontend/src/Components/Layout/Header/HeaderReview/styles.ts
+++ frontend/src/Components/Layout/Header/HeaderReview/styles.ts
@@ -1,14 +1,43 @@
 import styled from "styled-components";
-import { H2 } from "../../../styled/Components";
-import { textColorMain, fontH2, blackTextStroke } from "../../../styled/Mixins/mixins";
+import { H2, Div, Section } from "../../../styled/Components";
+import {
+	textColorMain,
+	fontH2,
+	blackTextStroke,
+	zIndexMiddle,
+} from "../../../styled/Mixins/mixins";
 
 export const H2FilmTitle = styled(H2)`
-  max-width: 750px;
-  max-height: 85px;
-  overflow: hidden;
+	max-width: 750px;
+	max-height: 85px;
+	overflow: hidden;
 
-  ${textColorMain}
-  ${fontH2}
+	${textColorMain}
+	${fontH2}
   ${blackTextStroke}
 `;
 
+export const DivFilmInfo = styled(Div)`
+	align-items: center;
+	position: relative;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+`;
+
+export const DivFilmCard = styled(Div)`
+	max-width: 1300px;
+	display: flex;
+
+	flex-direction: column;
+	margin-left: auto;
+	margin-right: auto;
+`;
+
+export const SectionHero = styled(Section)`
+position: relative;
+
+max-width: 1440px;
+min-height: 375px;
+  ${zIndexMiddle}
+`;
