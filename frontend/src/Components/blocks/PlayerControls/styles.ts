d455ed68275151bca6a5b64229c08fb870d3f5f7
fess986u <fess986@mail.ru>
2024-08-12 20:07:21 +0300

    Стилизация PlayerControls по styled  #128

---
+++ frontend/src/Components/blocks/PlayerControls/styles.ts
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import { Div } from "../../styled/Components";
+import { textColorMain, zIndexMiddle } from "../../styled/Mixins/mixins";
+
+export const DivConrolRow = styled(Div)`
+	display: flex;
+
+	flex-direction: row;
+
+	align-items: center;
+	margin-bottom: 15px;
+`;
+
+export const DivControlsTime = styled(Div)`
+	flex-shrink: 0;
+
+	width: 60px;
+
+	text-align: right;
+
+	${textColorMain}
+	font-size: 14px;
+`;
+
+export const DivControlsName = styled(Div)`
+	${textColorMain}
+	font-size: 17px;
+	line-height: 20px;
+	font-weight: 700;
+
+	margin: 0 30px;
+`;
+
+export const DivControlsContainer = styled(Div)`
+	position: absolute;
+
+	${zIndexMiddle}
+	left: 0;
+	right: 0;
+	bottom: 0;
+
+	padding: 25px;
+`;
