14097eb9d41227e662070f168435f7bf83f0424b
fess986u <fess986@mail.ru>
2024-08-14 17:08:06 +0300

    Удалены ненужные заккоментированные части  #44

--- frontend/src/Components/Layout/Header/HeaderFilmCard/HeaderFilmCard.tsx
+++ frontend/src/Components/Layout/Header/HeaderFilmCard/HeaderFilmCard.tsx
@@ -16,34 +16,6 @@ const HeaderFilmCard: React.FC<HeaderFilmCardProps> = ( {currentFilm, isAuth} ) 
       <H1Hidden>{`${currentFilm.name} Page`}</H1Hidden>
       <Header isAuth={isAuth}/>
       <FilmInfoCard film={currentFilm} />
-          {/* <div className="movie-page__film-card film-card">
-            <div className="movie-page__film-card-info film-card__info">
-              <div className="film-card__description">
-                <h2 className="film-card__title">The Grand Budapest Hotel</h2>
-                <p className="film-card__meta">
-                  <span className="film-card__genre">Drama</span>
-                  <span className="film-card__year">2014</span>
-                </p>
-                <p className="film-card__meta">
-                  <span className="film-card__rating">Film rating: 8.1</span>
-                </p>
-                <div className="film-card__buttons">
-                  <button className="film-card__button button button--play" type="button">
-                    <svg viewBox="0 0 19 19" width={19} height={19}>
-                      <use xlinkHref="#play-s" />
-                    </svg>
-                    <span>Play</span>
-                  </button>
-                  <button className="film-card__button button button--list" type="button">
-                    <svg viewBox="0 0 19 20" width={19} height={20}>
-                      <use xlinkHref="#add" />
-                    </svg>
-                    <span>My list</span>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div> */}
         </section>
   )
 };
