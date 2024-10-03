86af083b2f686e9949632fd3d1eb36c05168a98e
fess986u <fess986@mail.ru>
2024-10-03 20:35:17 +0300

    Добавлена фейковая задержка #7
    - для эмуляции загрузки данных по сети добавлена
    фейковая задержка через объект axios
    - так же добавлена фейковая загрузка фильмов
    в компоненте App (но пока не использована)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,24 @@
+import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import { useSelector } from 'react-redux';
 import { getFilmList } from './store/films/filmsSelector';
 import { getIsAuth } from './store/user/userSelectors';
+import api from './api/api';
+import { Films } from './mock/films';
 
 import useRoutes from './hooks/useRoutes';
 
 function App() {
+
+  useEffect(() => {
+    const fetchFunc = async () => {
+      const filmsF = await api.get('/films').then(() => Films)
+      console.log(filmsF);
+    }
+
+    fetchFunc()
+  }, []);
+
   const films = useSelector(getFilmList);
   const isAuth = useSelector(getIsAuth);
   
