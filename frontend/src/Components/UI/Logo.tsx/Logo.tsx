5146bb2bcc9217e7219a896330871b2ad8271189
fess986u <fess986@mail.ru>
2024-10-07 19:49:02 +0300

    Логика нажатия кнопки Logo #89
    - происходит переход на главную страницу
    - устанавливается исходное количество фильмов

--- frontend/src/Components/UI/Logo.tsx/Logo.tsx
+++ frontend/src/Components/UI/Logo.tsx/Logo.tsx
@@ -1,13 +1,21 @@
 import { AppRoutes } from "../../../const/const";
 import { LinkLogo, ImgLogo } from "./styled";
+import { resetFilmsShownCount } from "../../../store/app/appSlice";
+import { useAppDispatch } from "../../../store";
 
 type LogoProps = {
   footer?: boolean,
 }
 
 const Logo: React.FC<LogoProps> = ({ footer }) => {
+  const dispatch = useAppDispatch();
+
+  const handleClick = () => {
+    dispatch(resetFilmsShownCount())
+  }
+
   return (
-    <LinkLogo to={AppRoutes.ROOT} $footer={footer}>
+    <LinkLogo onClick={handleClick} to={AppRoutes.ROOT} $footer={footer}>
       <ImgLogo src="/images/logo.jpeg" alt="Films Library" />
     </LinkLogo>
   )
