16fcf8e9473421177eda3a32cb45024c3d98d334
fess986u <fess986@mail.ru>
2024-10-07 18:57:53 +0300

    Перемещена вверх логика количества фильмов #160
    - для того чтобы проще было переиспользовать
    компонент FilmsList перемещена логика отображения
    количества фильмов на уровень выше

--- frontend/src/Components/blocks/FilmList/FilmList.tsx
+++ frontend/src/Components/blocks/FilmList/FilmList.tsx
@@ -1,10 +1,8 @@
 import SmallFilmCard from "../../UI/SmallFilmCard/SmallFilmCard";
 import { DivFilmList } from "./styles";
 
-import { getFilmsShownCount } from "../../../store/app/appSelectors";
 import { FilmProps } from "../../../types/types";
 
-import { useSelector } from "react-redux";
 
 type FilmListProps = {
   films: FilmProps[]
@@ -12,11 +10,10 @@ type FilmListProps = {
 
 const FilmList: React.FC<FilmListProps> = ({ films }) => {
 
-  const filmsShownCount = useSelector(getFilmsShownCount);  // получаем количество отображаемых фильмов
-
   return (
     <DivFilmList>
-      {films.slice(0, filmsShownCount).map(film => <SmallFilmCard key={film.id} film={film} />)}
+      {films && films.map(film => <SmallFilmCard key={film.id} film={film} />)}
+      {!films && <div>No films to show...</div>}
     </DivFilmList>
   )
 }
