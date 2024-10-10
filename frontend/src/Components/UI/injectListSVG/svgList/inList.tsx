53fe2f5fda46667b48b85cfeb85004424445c325
fess986u <fess986@mail.ru>
2024-10-10 19:28:12 +0300

    Добавление начальной логики в ButtonAdd #160

--- frontend/src/Components/UI/injectListSVG/svgList/inList.tsx
+++ frontend/src/Components/UI/injectListSVG/svgList/inList.tsx
@@ -1,10 +1,7 @@
 const InList = (): JSX.Element => {
   return (
-    <symbol id="full-screen" viewBox="0 0 27 27">
-      <path fillRule="evenodd" clipRule="evenodd" d="M23.8571 0H16V3.14286H23.8571V11H27V3.14286V0H23.8571Z" fill="#FFF9D9" fillOpacity="0.7" />
-      <path fillRule="evenodd" clipRule="evenodd" d="M27 23.8571V16H23.8571V23.8571H16V27H23.8571H27L27 23.8571Z" fill="#FFF9D9" fillOpacity="0.7" />
-      <path fillRule="evenodd" clipRule="evenodd" d="M0 3.14286L0 11H3.14286L3.14286 3.14286L11 3.14286V0H3.14286H0L0 3.14286Z" fill="#FFF9D9" fillOpacity="0.7" />
-      <path fillRule="evenodd" clipRule="evenodd" d="M3.14286 27H11V23.8571H3.14286L3.14286 16H0L0 23.8571V27H3.14286Z" fill="#FFF9D9" fillOpacity="0.7" />
+    <symbol id="in-list" viewBox="0 0 18 14">
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M2.40513 5.35353L6.1818 8.90902L15.5807 0L18 2.80485L6.18935 14L0 8.17346L2.40513 5.35353Z" fill="#EEE5B5" />
     </symbol>
   )
 }
