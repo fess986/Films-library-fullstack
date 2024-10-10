fd8c3e1ffcbaa335af7d39e8a0bdaceda5fed771
fess986u <fess986@mail.ru>
2024-10-10 19:11:45 +0300

    Добавлена кнопка "ButtonDetails" #160
    - для того чтобы можно было переходить на
    страницу с фильмом

--- frontend/src/Components/blocks/FilmButtons/FilmButtons.tsx
+++ frontend/src/Components/blocks/FilmButtons/FilmButtons.tsx
@@ -1,15 +1,18 @@
 import ButtonAdd from "../../UI/Buttons/ButtonAdd/ButtonAdd";
 import ButtonPlay from "../../UI/Buttons/ButtonPlay/ButtonPlay";
+import ButtonDetails from "../../UI/Buttons/ButtonDetails/ButtonDetails";
 import { DivFilmButtons } from "./styles";
 
 type FilmButtonsProps = {
-  callback: () => void
+  callback: () => void,
+  details?: boolean,
 }
-const FilmButtons: React.FC<FilmButtonsProps> = ( {callback} ) => {
+const FilmButtons: React.FC<FilmButtonsProps> = ( {callback, details} ) => {
   return (
     <DivFilmButtons>
       <ButtonPlay id={1}/>
       <ButtonAdd callback={callback} />
+      {details && <ButtonDetails  />}
     </DivFilmButtons>
   )
 }
