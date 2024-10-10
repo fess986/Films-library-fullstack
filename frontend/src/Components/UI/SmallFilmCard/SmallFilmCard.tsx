d567dae0c424dfa88156ba2da5495de38bb47e8d
fess986u <fess986@mail.ru>
2024-10-10 20:08:36 +0300

    Логика кликов SmallFilmCard #160
    - при клике по изображению устанавливаем активным
    фильмом - тот фильм, по которому произошло
     нажатие, что будет вызывать перерисовку
     главной страницы
    - при клике на ссылку фильма - устанавливаем
    текущий фиьльм активным и переходим по ссылке

--- frontend/src/Components/UI/SmallFilmCard/SmallFilmCard.tsx
+++ frontend/src/Components/UI/SmallFilmCard/SmallFilmCard.tsx
@@ -1,18 +1,26 @@
 import { FilmProps } from "../../../types/types";
 import { SmallFilmCardLink, SmallFilmCardTitle, SmallFilmCardImg, SmallFilmCardContainer, SmallFilmCardArticle } from "./styles";
+import { setActiveFilm } from "../../../store/films/filmsSlice";
+import { useAppDispatch } from "../../../store";
 
 type SmallFilmCardProps = {
   film: FilmProps,
 }
 
 const SmallFilmCard: React.FC<SmallFilmCardProps> = ({ film }) => {
+const dispatch = useAppDispatch();
+
+  const handleClick = () => {
+    dispatch(setActiveFilm(film))
+  }
+
   return (
     <SmallFilmCardArticle>
-      <SmallFilmCardContainer>
+      <SmallFilmCardContainer onClick={handleClick}>
         <SmallFilmCardImg src={ film.previewImage } alt={film.name} />
       </SmallFilmCardContainer>
       <SmallFilmCardTitle>
-        <SmallFilmCardLink to={`/films/${film.id}`} >{film.name}</SmallFilmCardLink>
+        <SmallFilmCardLink onClick={handleClick} to={`/films/${film.id}`} >{film.name}</SmallFilmCardLink>
       </SmallFilmCardTitle>
     </SmallFilmCardArticle>
   )
