30ab5ba495525b838e72ce7d474dc70ef39eb35f
fess986u <fess986@mail.ru>
2024-11-10 18:08:25 +0300

    Исправление логики компонента ButtonAdd #168
    - теперь отображение кнопки зависит от статуса
    пользователя и от того, входит ли фильм в
    список любимых

--- frontend/src/Components/UI/Buttons/ButtonAdd/ButtonAdd.tsx
+++ frontend/src/Components/UI/Buttons/ButtonAdd/ButtonAdd.tsx
@@ -1,11 +1,52 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+import { getIsAuth, getFavoriteFilms } from "../../../../store/user/userSelectors";
+import { getActiveFilm } from "../../../../store/films/filmsSelector";
+import { AuthStatus, AppRoutes } from "../../../../const/const";
+
 import { StyledButton } from "../styles";
-import { useState } from "react";
 
 const ButtonAdd : React.FC = (  ) => {
   const [added, setAdded] = useState(false);
 
+  const navigate = useNavigate();
+
+  const isAuth = useSelector(getIsAuth);
+  console.log('isAuth', isAuth)
+  const favoriteFilmListId = useSelector(getFavoriteFilms);
+  console.log('favoriteFilmListId', favoriteFilmListId)
+
+  const activeFilm = useSelector(getActiveFilm);
+  console.log('activeFilm', activeFilm)
+
+  useEffect(() => {
+    if (isAuth === AuthStatus.NO_AUTH || isAuth === AuthStatus.UNKNOWN) {
+      setAdded(false);
+    }
+
+    if (isAuth === AuthStatus.AUTH) {
+      if (activeFilm === null) {
+        setAdded(false);
+        return
+      }
+
+      if (favoriteFilmListId.includes(activeFilm.id)) {
+        setAdded(true);
+      } else {
+        setAdded(false);
+      }
+    }
+
+  }, [isAuth, activeFilm, favoriteFilmListId]);
+
   const handleClick = () => {
+    if (isAuth === AuthStatus.NO_AUTH || isAuth === AuthStatus.UNKNOWN) {
+      navigate(AppRoutes.SIGN_IN);
+      return;
+    }
+
     setAdded(!added);  // тут будет логика проверки есть ли фильм в списке любимых TODO
     console.log('добавить логику добавления фильма в списоок любимых') 
   } 
