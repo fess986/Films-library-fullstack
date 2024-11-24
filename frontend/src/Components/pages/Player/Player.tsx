4faf881b1a79e1dd758a9f971155cc03e440dbbd
Karpov Maxim <fess986@mail.ru>
2024-11-24 12:40:00 +0300

    Получение ссылки на тег video #222
    - для того чтобы дотянуться во вложенный
    компонент, используем forwardRef

--- frontend/src/Components/pages/Player/Player.tsx
+++ frontend/src/Components/pages/Player/Player.tsx
@@ -1,3 +1,5 @@
+import React, { useState, useRef } from "react";
+
 import Video from "../../UI/Video/Video";
 import ButtonPlayerExit from "../../UI/Buttons/ButtonPlayerExit/ButtonPlayerExit";
 import PlayerControls from "../../blocks/PlayerControls/PlayerControls";
@@ -5,25 +7,39 @@ import PlayerControls from "../../blocks/PlayerControls/PlayerControls";
 import useActiveFilm from "../../../hooks/useActiveFilm";
 
 import { FilmProps } from "../../../types/types";
-import {DivPlayerContainer} from "./styles";
+import { DivPlayerContainer } from "./styles";
 
 type PlayerProps = {
   film: FilmProps
 }
 
 const Player: React.FC<PlayerProps> = () => {
+  const [isLoading, setIsloading] = useState(true);
+  const [isPlaying, setIsPlaing] = useState(false);
+  const [currentTimePlaying, setCurrentTimePlaying] = useState(0);
+  const [filmDuration, setfilmDuration] = useState(0);
+  const [playRowPosition, setPlayRowPosition] = useState(0);
+
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  console.log(videoRef)
+
   const { currentFilm, isActiveFilmLoaded, id } = useActiveFilm();
 
   console.log(id)
-  console.log(currentFilm)
-  console.log(isActiveFilmLoaded)
+  // console.log(currentFilm)
+  // console.log(isActiveFilmLoaded)
 
   // console.log(film)
 
   return (
     <DivPlayerContainer>
-      <Video poster={currentFilm?.playerImage ? currentFilm?.playerImage : '/images/player-poster.jpg'} src={currentFilm?.videoLink} 
-      onTimeUpdate={(evt) => {console.log(evt)}} onClick={() => {console.log('нажата кнопка плеера')}} />
+      <Video
+        ref={videoRef}
+        poster={currentFilm?.playerImage ? currentFilm?.playerImage : '/images/player-poster.jpg'}
+        src={currentFilm?.videoLink}
+        onTimeUpdate={(evt) => { console.log(evt) }}
+        onClick={() => { console.log('нажата кнопка плеера') }}
+      />
 
       <ButtonPlayerExit />
 
