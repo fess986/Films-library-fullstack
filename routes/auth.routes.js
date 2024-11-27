50f05c889c551d88cb7bef1a10319ab8b2df06a5
Karpov Maxim <fess986@mail.ru>
2024-11-27 20:33:58 +0300

    Валидация пользователей #234
    - при помощи пакета express-validator
    добавляем валидацию почты и пароля

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,10 +2,27 @@ import {Router} from 'express';
 const router = Router();
 import { User } from '../models/User';
 import bcrypt from 'bcryptjs';
+import { check, validationResult } from 'express-validator';
 
 // данный запрос будет конкатенироваться с /api/auth и получится /api/auth/register
-router.post('/register', async (req, res) => {
+// между названием роута /register - и функцией - мы можем вставить мидлвары
+router.post(
+  '/register',
+  [
+    check('email', 'Некорректный email').isEmail(),  // isEmail - это встроенный метод валидатора, который защищает от ввода некорректных данных почты
+    check('password', 'Минимальная длина пароля 6 символов').isLength({min: 6}),  // isLength - проверка длины
+  ],
+  async (req, res) => {
   try {
+    const errors = validationResult(req);  // таким образом мы подключаем валидацию
+
+    if (!errors.isEmpty()) {  // проверяем ли есть что в массиве ошибок
+      return res.status(400).json({  // возвращаем статус 400 - ошибка
+        errors: errors.array(),  // массив ошибок
+        message: 'Некорректные данные при регистрации'  // сообщение об ошибке
+      })
+    }
+
     const {email, password} = req.body;
 
     const candidate = await User.findOne({email: email}); // проверяем есть ли пользователь с таким email в базе данных
