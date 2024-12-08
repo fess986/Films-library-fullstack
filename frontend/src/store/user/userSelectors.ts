f705ec4efe0c61990286fe75612ceee0f0273c2f
Karpov Maxim <fess986@mail.ru>
2024-12-08 13:24:50 +0300

    Перевод логики управления токена на redux #246

--- frontend/src/store/user/userSelectors.ts
+++ frontend/src/store/user/userSelectors.ts
@@ -5,3 +5,4 @@ import { RootState } from "../index";
 export const getIsAuth = (state: RootState) => state[StoreNames.User].isAuth;
 export const getFavoriteFilms = (state: RootState) => state[StoreNames.User].favoriteFilms;
 export const getUserId = (state: RootState) => state[StoreNames.User].userId;
+export const getToken = (state: RootState) => state[StoreNames.User].token;
--- frontend/src/store/user/userSlice.ts
+++ frontend/src/store/user/userSlice.ts
@@ -6,10 +6,12 @@ type UserState = {
   isAuth: AuthStatus;
   favoriteFilms: number[];
   userId: number | null;
+  token: string | null;
 };
 
 const initialUserState: UserState = {
   isAuth: AuthStatus.UNKNOWN,
+  token: null,
   favoriteFilms: [],
   userId: null,
 };
@@ -33,7 +35,10 @@ export const userSlice = createSlice({
     setUserId: (state, action: PayloadAction<number | null>) => {
       state.userId = action.payload;
     },
+    setToken: (state, action: PayloadAction<string | null>) => {
+      state.token = action.payload;
+    }
   },
 })
 
-export const { setAuthStatus, setFavoriteFilms, setUserId, addToFavoriteFilm, removeFromFavoriteFilm } = userSlice.actions;
+export const { setAuthStatus, setFavoriteFilms, setUserId, addToFavoriteFilm, removeFromFavoriteFilm, setToken } = userSlice.actions;
