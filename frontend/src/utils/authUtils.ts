fea86591b80b44df472e6046e385bf1bb21134cb
Karpov Maxim <fess986@mail.ru>
2025-01-15 19:01:28 +0300

    Исправлена функция логина #270
    - теперь при логине так же получаем список
    любимых фильмов с БД

--- frontend/src/utils/authUtils.ts
+++ frontend/src/utils/authUtils.ts
@@ -1,17 +1,20 @@
 import { AuthStatus, storageName } from '../const/const'
 import { AppDispatch } from '../store'
 import { resetFilmsShownCount } from '../store/app/appSlice'
-import { setToken, setUserId, setAuthStatus } from '../store/user/userSlice'
+import { setToken, setUserId, setAuthStatus, setFavoriteFilms } from '../store/user/userSlice'
 
 export const loginUtil = (
   dispatch: AppDispatch,
   jwtToken: string,
-  id: string
+  id: string,
+  favoriteFilms: string[]
 ): void => {
+  console.log(favoriteFilms)
   // Диспатчим действия в Redux
   dispatch(setToken(jwtToken))
   dispatch(setUserId(id))
   dispatch(setAuthStatus(AuthStatus.AUTH))
+  dispatch(setFavoriteFilms(favoriteFilms))
 
   // Сохраняем данные в localStorage
   localStorage.setItem(
@@ -19,6 +22,7 @@ export const loginUtil = (
     JSON.stringify({
       userId: id,
       token: jwtToken,
+      favoriteFilms: favoriteFilms,
     })
   )
 }
@@ -28,6 +32,7 @@ export const logoutUtil = (dispatch: AppDispatch): void => {
   dispatch(setUserId(null))
   dispatch(setAuthStatus(AuthStatus.NO_AUTH))
   dispatch(resetFilmsShownCount())
+  dispatch(setFavoriteFilms([]))
 
   localStorage.removeItem(storageName)
 }
@@ -35,7 +40,7 @@ export const logoutUtil = (dispatch: AppDispatch): void => {
 export const checkAuthUtil = (dispatch: AppDispatch): AuthStatus => {
   const data = JSON.parse(localStorage.getItem(storageName) || '{}')
   if (data && data.token) {
-    loginUtil(dispatch, data.token, data.userId)
+    loginUtil(dispatch, data.token, data.userId, data.favoriteFilms)
     return AuthStatus.AUTH
   }
   return AuthStatus.NO_AUTH
