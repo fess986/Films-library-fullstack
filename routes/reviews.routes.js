6074e1aa6f8b079db5c53f16a6d6a528e5f34f96
Karpov Maxim <fess986@mail.ru>
2025-01-19 17:32:48 +0300

    editing...

--- routes/reviews.routes.js
+++ routes/reviews.routes.js
@@ -1,21 +1,23 @@
-import { Router } from "express";
+import { Router } from 'express'
 
-import { Review } from "../models/Reviews.js";
+import { Review } from '../models/Reviews.js'
 
-const router = Router();
+const router = Router()
 
-router.post("/get-reviews", async (req, res) => {
+router.post('/get-reviews', async (req, res) => {
   // console.log(req)
   // console.log(res)
   try {
     console.log('запрос reviews')
     const reviews = await Review.find({})
     console.log(reviews)
-    res.status(200).json(reviews);
+    res.status(200).json(reviews)
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Что-то пошло не так при загрузке отзывов" });
+    console.log(error)
+    res
+      .status(500)
+      .json({ message: 'Что-то пошло не так при загрузке отзывов' })
   }
 })
 
-export default router;
+export default router
