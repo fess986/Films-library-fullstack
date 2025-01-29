e06d251ff55d7183586650ba38e9a683597151b8
Karpov Maxim <fess986@mail.ru>
2025-01-29 18:38:41 +0300

    Кнопка перехода на страницу пользователя #160
    - добавлена сама кнопка в проект
    - добавлена базовая логика отображения на
    главной странице

---
+++ frontend/src/Components/UI/Buttons/ButtonUserPage/ButtonUserPage.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+// import { useSelector } from 'react-redux'
+// import { useNavigate } from 'react-router-dom'
+
+// import { AppRoutes } from '../../../../const/const'
+// import { useAppDispatch } from '../../../../store'
+// import { resetFilmsShownCount } from '../../../../store/app/appSlice'
+// import { getActiveFilm } from '../../../../store/films/filmsSelector'
+import { StyledButton } from '../styles'
+
+const ButtonUserPage: React.FC = () => {
+  // const navigate = useNavigate()
+  // const dispatch = useAppDispatch()
+  // const currentFilm = useSelector(getActiveFilm)
+
+  const handleClick = () => {
+    console.log('click')
+    // currentFilm &&
+    //   navigate(
+    //     `${AppRoutes.ROOT}${AppRoutes.FILM_CARD.replace(':id/*', String(currentFilm.id))}`
+    //   )
+    // dispatch(resetFilmsShownCount())
+  }
+
+  return (
+    <StyledButton onClick={handleClick}>
+      <svg viewBox="0 0 25 25" width={24} height={25}>
+        <use xlinkHref="#add" />
+      </svg>
+      <span>User Page</span>
+    </StyledButton>
+  )
+}
+
+export default ButtonUserPage
--- frontend/src/Components/blocks/FilmButtons/FilmButtons.tsx
+++ frontend/src/Components/blocks/FilmButtons/FilmButtons.tsx
@@ -3,15 +3,18 @@ import ButtonAdd from '../../UI/Buttons/ButtonAdd/ButtonAdd'
 import ButtonAddReview from '../../UI/Buttons/ButtonAddReview/ButtonAddReview'
 import ButtonDetails from '../../UI/Buttons/ButtonDetails/ButtonDetails'
 import ButtonPlay from '../../UI/Buttons/ButtonPlay/ButtonPlay'
+import ButtonUserPage from '../../UI/Buttons/ButtonUserPage/ButtonUserPage'
 
 type FilmButtonsProps = {
   details?: boolean
   addReview?: boolean
+  userPage?: boolean
   id: string
 }
 const FilmButtons: React.FC<FilmButtonsProps> = ({
   details,
   addReview,
+  userPage,
   id,
 }) => {
   return (
@@ -20,6 +23,7 @@ const FilmButtons: React.FC<FilmButtonsProps> = ({
       <ButtonAdd />
       {details && <ButtonDetails />}
       {addReview && <ButtonAddReview />}
+      {userPage && <ButtonUserPage />}
     </DivFilmButtons>
   )
 }
