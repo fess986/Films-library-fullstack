b99ed2d9a34c8de174336bb31b302737195b2bbb
Karpov Maxim <fess986@mail.ru>
2025-01-29 17:17:50 +0300

    Очистка от консолей #14

--- middlewares/isauth.middleware.js
+++ middlewares/isauth.middleware.js
@@ -10,15 +10,12 @@ export default (req, res, next) => {
   try {
     const token = req?.headers?.authorization?.split(' ')[1] // токен будет храниться в хедере в виде "Bearer token_value"
 
-    console.log('token - ', token)
-
     if (!token) {
       return res.status(401).json({ message: 'Вы не авторизованы!' })
     }
 
     // получаем закодированное значение {userId: 'id'}
     const decoded = jwt.verify(token, config.get('jwtToken'))
-    console.log('decoded ---', decoded)
     req.user = decoded // записываем в объект запроса req.user {userId: 'id'}
     // console.log(req.user)
     next() // продолжаем скрипт
