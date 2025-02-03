fc0130dcf6d87b6f6cda71a4a0f493e868e6e702
Karpov Maxim <fess986@mail.ru>
2025-02-03 20:25:13 +0300

    Настройка vitest и написание первого #7
    - редактирование конфигурационных файлов
    - добавление скриптов для тестирования
    - написание первого теста для utils.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +7,12 @@ export default defineConfig({
     'process.env.NODE_ENV': `"${process.env.NODE_ENV || 'development'}"`,
   },
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    coverage: {
+      provider: 'istanbul',
+    },
+  },
 })
