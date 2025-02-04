86f9d9aac2f4854d882fc8a1d9bd728432144ad2
Karpov Maxim <fess986@mail.ru>
2025-02-04 11:24:30 +0300

    editing...

--- frontend/src/utils/utils.test.ts
+++ frontend/src/utils/utils.test.ts
@@ -1,4 +1,9 @@
-import { getDuration, parseCommentDate, formatDate, parseSeconds } from './utils'
+import {
+  getDuration,
+  parseCommentDate,
+  formatDate,
+  parseSeconds,
+} from './utils'
 
 describe('getDuration', () => {
   test('корректно форматирует минуты в часы и минуты', () => {
@@ -20,28 +25,28 @@ describe('parseCommentDate', () => {
   test('корректно форматирует дату', () => {
     expect(parseCommentDate('2022-01-01')).toBe('January 1, 2022')
   }),
-  test('корректно форматирует дату', () => {
-    expect(parseCommentDate('2022-11-11')).toBe('November 11, 2022')
-  })
+    test('корректно форматирует дату', () => {
+      expect(parseCommentDate('2022-11-11')).toBe('November 11, 2022')
+    })
 })
 
 describe('formatDate', () => {
   test('корректно форматирует дату', () => {
     expect(formatDate('2022-01-01')).toBe('2022-01-01')
   }),
-  test('корректно форматирует дату', () => {
-    expect(formatDate('2022-11-11')).toBe('2022-11-11')
-  })
+    test('корректно форматирует дату', () => {
+      expect(formatDate('2022-11-11')).toBe('2022-11-11')
+    })
 })
 
 describe('parseSeconds', () => {
   test('корректно форматирует время до часов - 1', () => {
     expect(parseSeconds(90)).toBe('01:30')
   }),
-  test('корректно форматирует время до часов - 2', () => {
-    expect(parseSeconds(45)).toBe('00:45')
-  }),
-  test('корректно форматирует время с часами', () => {
-    expect(parseSeconds(4000)).toBe('01:06:40')
-  })
+    test('корректно форматирует время до часов - 2', () => {
+      expect(parseSeconds(45)).toBe('00:45')
+    }),
+    test('корректно форматирует время с часами', () => {
+      expect(parseSeconds(4000)).toBe('01:06:40')
+    })
 })
