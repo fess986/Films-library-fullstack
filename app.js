26598c407867b3686541b8ae716572218100461e
fess <fess986@mail.ru>
2025-05-07 19:40:26 +0300

    editing...

--- app.js
+++ app.js
@@ -3,19 +3,17 @@ import config from 'config'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import path from 'path'
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'url'
 import { ApiRoutes } from './const/const.js'
 import { corsOptions } from './middlewares/corsConfig.middleware.js'
 
 // так как в ES6 модулях нет доступа к __dirname, мы можем его получить через fileURLToPath
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
 const port = config.get('PORT') || 4000
 console.log(config.get('ENV'))
-
-
 
 // Настройка CORS через настроенный в отдельном файле объект
 // теперь при запуске в дев-режиме будет обращение к порту локала 5173, для билда - 4173, а для прода - адрес домена
@@ -30,7 +28,6 @@ app.use(ApiRoutes.REVIEWS, (await import('./routes/reviews.routes.js')).default)
 
 // в случае если приложение запускается в продакшен режиме, то будет обращение к папке frontend/dist, где будет лежать фронтэнд проекта
 if (process.env.NODE_ENV === 'production') {
-
   app.use('/', express.static(path.join(__dirname, 'frontend', 'dist')))
 
   app.get('*', (req, res) => {
